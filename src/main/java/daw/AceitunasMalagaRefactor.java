f37a22c89c99ef30f48f1abe1398345eda0712a4
Eduardo <eduardocorreaaranda10@gmail.com>
2023-10-23 20:15:57 +0200

    hola buenas

--- src/main/java/daw/AceitunasMalagaRefactor.java
+++ src/main/java/daw/AceitunasMalagaRefactor.java
@@ -19,26 +19,39 @@ public class AceitunasMalagaRefactor {
         final double PRECIO_INICIAL = 2.45, G_PIC = 0.3, F_PIC = 0.2,
                 F_ALO = 0.3, G_ALO = 0.15, F_HOJ = 0.05, G_HOJ = 0.15;
         String tamaño, repetirProg;
-        int opcion = 0;
+        int opcion = 0, kAceit = 0;
         double resultado;
-
+        boolean operacionCorrecta = true;
         do {
-            try {
-                System.out.println("""
-                            ¿Qué tipo de aceitunas vendes?: 
+            do {
+                System.out.print(""" 
                             1º Picual
                             2º Aloreña 
                             3º Hojiblanca
+                            ¿Qué tipo de aceitunas vendes: ?
                              """);
-                opcion = teclado.nextInt();
-            } catch (InputMismatchException ime) {
-                System.out.println("Debes de introducir uno de los datos "
-                        + "del menu");
-            }
+                try {
+                    opcion = teclado.nextInt();
+                    operacionCorrecta = false;
+                } catch (InputMismatchException ime) {
+                    System.out.println("Debes de introducir uno de los datos "
+                            + "del menu (1 - 2 - 3)");
+                    teclado.nextLine();
+                }
+            } while (operacionCorrecta || (opcion < 1 || opcion > 3));
             switch (opcion) {
                 case 1 -> {
-                    System.out.println("¿Cuantos kilos de aceitunas vas ha entregar: ?");
-                    int kAceit = teclado.nextInt();
+                    do {
+                        System.out.println("¿Cuantos kilos de aceitunas vas "
+                                + "ha entregar: ?");
+                        try {
+                            kAceit = teclado.nextInt();
+                            operacionCorrecta = false;
+                        } catch (InputMismatchException ime) {
+                            System.out.println("Introduce los datos correctos ");
+                            teclado.nextLine();
+                        }
+                    } while (operacionCorrecta || kAceit < 100);
                     teclado.nextLine();
                     System.out.println("¿Son gruesas o finas: ?");
                     tamaño = teclado.nextLine();
@@ -46,21 +59,30 @@ public class AceitunasMalagaRefactor {
                         case "1", "gruesas" -> {
                             resultado = (kAceit * PRECIO_INICIAL) * G_PIC;
                             System.out.printf("El precio de las aceitunas "
-                                    + "gruesas es: %.2f", resultado);
+                                    + "gruesas es: %.2f\n", resultado);
                         }
                         case "2", "finas" -> {
                             resultado = (kAceit * PRECIO_INICIAL) * F_PIC;
                             System.out.printf("El precio de las aceitunas "
-                                    + "finas es: %.2f", resultado);
+                                    + "finas es: %.2f\n", resultado);
                         }
                         default -> {
-                            System.out.println("Esa no es una de las opciones");
+                            System.out.println("Esa no es una de las opciones\n");
                         }
                     }
                 }
                 case 2 -> {
-                    System.out.println("¿Cuantos kilos de aceitunas vas ha entregar: ?");
-                    int kAceit = teclado.nextInt();
+                    do {
+                        System.out.println("¿Cuantos kilos de aceitunas vas "
+                                + "ha entregar: ?");
+                        try {
+                            kAceit = teclado.nextInt();
+                            operacionCorrecta = false;
+                        } catch (InputMismatchException ime) {
+                            System.out.println("Introduce los datos correctos ");
+                            teclado.nextLine();
+                        }
+                    } while (operacionCorrecta || kAceit < 100);
                     teclado.nextLine();
                     System.out.println("¿Son gruesas o finas: ?");
                     tamaño = teclado.nextLine();
@@ -68,21 +90,30 @@ public class AceitunasMalagaRefactor {
                         case "1", "gruesas" -> {
                             resultado = (kAceit * PRECIO_INICIAL) / G_ALO;
                             System.out.printf("El precio de las aceitunas "
-                                    + "gruesas es: %.2f", resultado);
+                                    + "gruesas es: %.2f\n", resultado);
                         }
                         case "2", "finas" -> {
                             resultado = (kAceit * PRECIO_INICIAL) / F_ALO;
                             System.out.printf("El precio de las aceitunas "
-                                    + "finas es: %.2f", resultado);
+                                    + "finas es: %.2f\n", resultado);
                         }
                         default -> {
-                            System.out.println("Esa no es una de las opciones");
+                            System.out.println("Esa no es una de las opciones\n");
                         }
                     }
                 }
                 case 3 -> {
-                    System.out.println("¿Cuantos kilos de aceitunas vas ha entregar: ?");
-                    int kAceit = teclado.nextInt();
+                    do {
+                        System.out.println("¿Cuantos kilos de aceitunas vas "
+                                + "ha entregar: ?");
+                        try {
+                            kAceit = teclado.nextInt();
+                            operacionCorrecta = false;
+                        } catch (InputMismatchException ime) {
+                            System.out.println("Introduce los datos correctos ");
+                            teclado.nextLine();
+                        }
+                    } while (operacionCorrecta || kAceit < 100);
                     teclado.nextLine();
                     System.out.println("¿Son gruesas o finas: ?");
                     tamaño = teclado.nextLine();
@@ -90,23 +121,19 @@ public class AceitunasMalagaRefactor {
                         case "1", "gruesas" -> {
                             resultado = (kAceit * PRECIO_INICIAL) * G_HOJ;
                             System.out.printf("El precio de las aceitunas "
-                                    + "gruesas es: %.2f", resultado);
+                                    + "gruesas es: %.2f\n", resultado);
                         }
                         case "2", "finas" -> {
                             resultado = (kAceit * PRECIO_INICIAL) / F_HOJ;
                             System.out.printf("El precio de las aceitunas "
-                                    + "finas es: %.2f", resultado);
+                                    + "finas es: %.2f\n", resultado);
                         }
                         default -> {
-                            System.out.println("Esa no es una de las opciones");
+                            System.out.println("Esa no es una de las opciones\n");
                         }
                     }
                 }
-                default -> {
-                    System.out.println("Error ");
-                }
             }
-            teclado.nextLine();
             System.out.println("¿Hay mas alguien mas en la cola de la almazara"
                     + " para entrega más aceitunas? ");
             repetirProg = teclado.nextLine();
